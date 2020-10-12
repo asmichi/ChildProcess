666e8bb19d98c3765aac3f7f0a8b93bb3ec090a0
asmichi <asmichi@users.noreply.github.com>
2020-10-12 17:58:42 +0900

    Verify quoted command lines by parsing them back

--- src/ChildProcess.Test/Utilities/WindowsCommandLineUtilTest.cs
+++ src/ChildProcess.Test/Utilities/WindowsCommandLineUtilTest.cs
@@ -1,11 +1,13 @@
 // Copyright (c) @asmichi (https://github.com/asmichi). Licensed under the MIT License. See LICENCE in the project root for details.
 
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using Xunit;
 
-namespace Asmichi.Utilities
+namespace Asmichi.Utilities.PlatformAbstraction.Windows
 {
-    public class WindowsCommandLineUtilTest
+    public class CommandLineUtilTest
     {
         [Fact]
         public void MakeCommandLineQuotesArguments()
@@ -39,9 +41,104 @@ namespace Asmichi.Utilities
             {
                 static string Replace(string s) => s.Replace('\'', '"');
 
-                Xunit.Assert.Equal(
-                    Replace(expected),
-                    WindowsCommandLineUtil.MakeCommandLine(Replace(fileName), args.Select(Replace).ToArray()).ToString());
+                var commandLine = WindowsCommandLineUtil.MakeCommandLine(Replace(fileName), args.Select(Replace).ToArray()).ToString();
+
+                Xunit.Assert.Equal(Replace(expected), commandLine);
+                Xunit.Assert.Equal(new[] { fileName }.Concat(args).Select(Replace).ToArray(), ParseCommandLine(commandLine).ToArray());
+            }
+        }
+
+        // NOTE: This implementation does not handle escape chars outside a quoted part. ex) a\"
+        private static List<string> ParseCommandLine(string commandLine)
+        {
+            bool IsDelimiter(char c) => c == ' ' || c == '\t';
+
+            var sb = new StringBuilder();
+            var result = new List<string>();
+
+            // Trim away preceeding and trailing delimiters.
+            int i = commandLine.TakeWhile(IsDelimiter).Count();
+            if (i == commandLine.Length)
+            {
+                return result;
+            }
+
+            int endIndex = commandLine.Length - commandLine.Reverse().TakeWhile(IsDelimiter).Count();
+
+            // split and unquote
+            while (true)
+            {
+                if (i == endIndex)
+                {
+                    result.Add(sb.ToString());
+                    return result;
+                }
+
+                var c = commandLine[i++];
+
+                if (IsDelimiter(c))
+                {
+                    // end of an argument
+                    result.Add(sb.ToString());
+                    sb.Clear();
+                }
+                else if (c == '"')
+                {
+                    // beginning of a quoted part
+                    int backslashes = 0;
+                    while (true)
+                    {
+                        if (i == endIndex)
+                        {
+                            if (backslashes > 0)
+                            {
+                                sb.Append('\\', backslashes);
+                            }
+                            break;
+                        }
+
+                        var c2 = commandLine[i++];
+                        if (c2 == '"')
+                        {
+                            if (backslashes % 2 == 0)
+                            {
+                                // end of a quoted part
+                                sb.Append('\\', backslashes / 2);
+                                break;
+                            }
+                            else
+                            {
+                                // an escaped double quote
+                                if (backslashes > 1)
+                                {
+                                    sb.Append('\\', backslashes / 2);
+                                }
+                                backslashes = 0;
+                                sb.Append('"');
+                            }
+                        }
+                        else if (c2 == '\\')
+                        {
+                            // buffer backslashes because they may escape a double quote or backslashes
+                            backslashes++;
+                        }
+                        else
+                        {
+                            // normal char
+                            if (backslashes > 0)
+                            {
+                                sb.Append('\\', backslashes);
+                                backslashes = 0;
+                            }
+                            sb.Append(c2);
+                        }
+                    }
+                }
+                else
+                {
+                    // normal char
+                    sb.Append(c);
+                }
             }
         }
     }
