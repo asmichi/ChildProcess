81e72be3bbb87a4504503d34855e4f8d2ec32ecf
asmichi <asmichi@users.noreply.github.com>
2021-01-30 20:08:26 +0900

    Add an example for *nix.

---
+++ src/ChildProcess.Example/ChildProcessExamplesUnix.cs
@@ -0,0 +1,107 @@
+// Copyright (c) @asmichi (https://github.com/asmichi). Licensed under the MIT License. See LICENCE in the project root for details.
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Asmichi.Utilities.ProcessManagement;
+
+namespace Asmichi.Utilities
+{
+    public static class ChildProcessExamplesUnix
+    {
+        public static async Task Run()
+        {
+            WriteHeader(nameof(BasicAsync));
+            await BasicAsync();
+
+            WriteHeader(nameof(RedirectionToFileAsync));
+            await RedirectionToFileAsync();
+
+            WriteHeader(nameof(WaitForExitAsync));
+            await WaitForExitAsync();
+        }
+
+        private static void WriteHeader(string name)
+        {
+            Console.WriteLine();
+            Console.WriteLine("*** {0}", name);
+        }
+
+        private static async Task BasicAsync()
+        {
+            var si = new ChildProcessStartInfo("sh", "-c", "echo foo")
+            {
+                StdOutputRedirection = OutputRedirection.OutputPipe,
+            };
+
+            using var p = ChildProcess.Start(si);
+            using (var sr = new StreamReader(p.StandardOutput!))
+            {
+                // "foo"
+                Console.Write(await sr.ReadToEndAsync());
+            }
+            await p.WaitForExitAsync();
+            // ExitCode: 0
+            Console.WriteLine("ExitCode: {0}", p.ExitCode);
+        }
+
+        private static async Task RedirectionToFileAsync()
+        {
+            var tempFile = Path.GetTempFileName();
+
+            var si = new ChildProcessStartInfo("ls", "/")
+            {
+                StdOutputRedirection = OutputRedirection.File,
+                StdOutputFile = tempFile,
+            };
+
+            using (var p = ChildProcess.Start(si))
+            {
+                await p.WaitForExitAsync();
+            }
+
+            // bin
+            // boot
+            // ...
+            Console.WriteLine(File.ReadAllText(tempFile));
+            File.Delete(tempFile);
+        }
+
+        // Truely asynchronous WaitForExitAsync: WaitForExitAsync does not consume a thread-pool thread.
+        // You will not need a dedicated thread for handling a child process.
+        // You can handle more processes than the number of threads.
+        private static async Task WaitForExitAsync()
+        {
+            const int N = 128;
+
+            var stopWatch = Stopwatch.StartNew();
+            var tasks = new Task[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                tasks[i] = SpawnCmdAsync();
+            }
+
+            // Spawned 128 processes.
+            // The 128 processes have exited.
+            // Elapsed Time: 3367 ms
+            Console.WriteLine("Spawned {0} 'sleep 3s' processes.", N);
+            await Task.WhenAll(tasks);
+            Console.WriteLine("The {0} processes have exited.", N);
+            Console.WriteLine("Elapsed Time: {0} ms", stopWatch.ElapsedMilliseconds);
+
+            static async Task SpawnCmdAsync()
+            {
+                var si = new ChildProcessStartInfo("sleep", "3s")
+                {
+                    StdInputRedirection = InputRedirection.ParentInput,
+                    StdOutputRedirection = OutputRedirection.NullDevice,
+                };
+
+                using var p = ChildProcess.Start(si);
+                await p.WaitForExitAsync();
+            }
+        }
+    }
+}
---
+++ src/ChildProcess.Example/ChildProcessExamplesWindows.cs
@@ -0,0 +1,113 @@
+// Copyright (c) @asmichi (https://github.com/asmichi). Licensed under the MIT License. See LICENCE in the project root for details.
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Asmichi.Utilities.ProcessManagement;
+
+namespace Asmichi.Utilities
+{
+    public static class ChildProcessExamplesWindows
+    {
+        public static async Task Run()
+        {
+            WriteHeader(nameof(BasicAsync));
+            await BasicAsync();
+
+            WriteHeader(nameof(RedirectionToFileAsync));
+            await RedirectionToFileAsync();
+
+            WriteHeader(nameof(WaitForExitAsync));
+            await WaitForExitAsync();
+        }
+
+        private static void WriteHeader(string name)
+        {
+            Console.WriteLine();
+            Console.WriteLine("*** {0}", name);
+        }
+
+        private static async Task BasicAsync()
+        {
+            var si = new ChildProcessStartInfo("cmd", "/C", "echo", "foo")
+            {
+                StdOutputRedirection = OutputRedirection.OutputPipe,
+            };
+
+            using var p = ChildProcess.Start(si);
+            using (var sr = new StreamReader(p.StandardOutput!))
+            {
+                // "foo"
+                Console.Write(await sr.ReadToEndAsync());
+            }
+            await p.WaitForExitAsync();
+            // ExitCode: 0
+            Console.WriteLine("ExitCode: {0}", p.ExitCode);
+        }
+
+        private static async Task RedirectionToFileAsync()
+        {
+            var tempFile = Path.GetTempFileName();
+
+            var si = new ChildProcessStartInfo("cmd", "/C", "set")
+            {
+                StdOutputRedirection = OutputRedirection.File,
+                StdOutputFile = tempFile,
+            };
+
+            using (var p = ChildProcess.Start(si))
+            {
+                await p.WaitForExitAsync();
+            }
+
+            // ALLUSERSPROFILE=C:\ProgramData
+            // ...
+            Console.WriteLine(File.ReadAllText(tempFile));
+            File.Delete(tempFile);
+        }
+
+        // Truely asynchronous WaitForExitAsync: WaitForExitAsync does not consume a thread-pool thread.
+        // You will not need a dedicated thread for handling a child process.
+        // You can handle more processes than the number of threads.
+        private static async Task WaitForExitAsync()
+        {
+            const int N = 128;
+
+            var stopWatch = Stopwatch.StartNew();
+            var tasks = new Task[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                tasks[i] = SpawnCmdAsync();
+            }
+
+            // Spawned 128 processes.
+            // The 128 processes have exited.
+            // Elapsed Time: 3367 ms
+            Console.WriteLine("Spawned {0} processes.", N);
+            await Task.WhenAll(tasks);
+            Console.WriteLine("The {0} processes have exited.", N);
+            Console.WriteLine("Elapsed Time: {0} ms", stopWatch.ElapsedMilliseconds);
+
+            async Task SpawnCmdAsync()
+            {
+                // Using timeout because Windows does not have a handy sleep command.
+                // (`powershell -NoProfile -Command Sleep 3` is not an option because
+                // it takes ~200 ms to boot.)
+                //
+                // This is super racy because these N timeout processes simultaneously
+                // attempts to perform PeekConsoleInput followed by ReadConsoleInput.
+                // When a console input arrives, some processes will get stuck in ReadConsoleInput.
+                var si = new ChildProcessStartInfo("timeout", "3")
+                {
+                    StdInputRedirection = InputRedirection.ParentInput,
+                    StdOutputRedirection = OutputRedirection.NullDevice,
+                };
+
+                using var p = ChildProcess.Start(si);
+                await p.WaitForExitAsync();
+            }
+        }
+    }
+}
